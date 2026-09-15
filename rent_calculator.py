51f3a95f7577dee385b4ffc15d538ebe4117de6d
Muhammad Samaan <msamaan1999@gmail.com>
2026-09-15 17:23:30 +0500

    formula and print comments added
    Added total expenses calculation and output for each person.

--- rent_calculator.py
+++ rent_calculator.py
@@ -15,8 +15,9 @@ chai_amount = int(input("Enter the chai amount: "))
 petrol_amount = int(input("Enter the petrol amount: "))
 electricity_amount = int(input("Enter the electricity amount: "))
 people = int(input("Enter the number of people: "))
-
+#Total expenses formula 
 total_expenses = rent_amount + food_amount + chai_amount + petrol_amount + electricity_amount
 amount_per_person = total_expenses / people
+#prininting the values 
 print(f"Total expenses: {total_expenses}")
 print(f"Amount each person has to pay: {amount_per_person}")
