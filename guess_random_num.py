32b0d2f445778e5b1689ee219565e4eee47156db
Muhammad Samaan <msamaan1999@gmail.com>
2026-09-17 22:22:43 +0500

    add exception handling

--- guess_random_num.py
+++ guess_random_num.py
@@ -1,12 +1,14 @@
 from random import randint
-
 ran_num = randint(1, 100)
 while True:
-    guess_num = int(input("Guess the number between 1 and 100 : "))
-    if (ran_num > guess_num):
-        print("number is too low")
-    elif (ran_num < guess_num):
-        print("number is too high")
-    else:
-        print(f"Congratulations You guess the right number {ran_num}")
-        break
+    try:
+        guess_num = int(input("Guess the number between 1 and 100 : "))
+        if (ran_num > guess_num):
+            print("number is too low")
+        elif (ran_num < guess_num):
+            print("number is too high")
+        else:
+            print(f"Congratulations You guess the right number {ran_num}")
+            break
+    except ValueError:
+        print("Enter some valid number")
