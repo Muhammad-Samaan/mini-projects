735758fc7e2f9b4becc57d07e6bf6da12f58713b
Muhammad Samaan <msamaan1999@gmail.com>
2026-09-17 21:40:36 +0500

    Rent Calculator modified

--- rent_calculator.py
+++ rent_calculator.py
@@ -1,13 +1,13 @@
-## Input needed 
+# Input needed
 # Rent amount
-# Food 
-# Chai 
-# Petrol 
+# Food
+# Chai
+# Petrol
 # Electricity
-# people 
+# people
 
-## output
-#display the total rent and the amount each person has to pay
+# output
+# display the total rent and the amount each person has to pay
 
 rent_amount = int(input("Enter the rent amount: "))
 food_amount = int(input("Enter the food amount: "))
@@ -15,9 +15,10 @@ chai_amount = int(input("Enter the chai amount: "))
 petrol_amount = int(input("Enter the petrol amount: "))
 electricity_amount = int(input("Enter the electricity amount: "))
 people = int(input("Enter the number of people: "))
-#Total expenses formula 
-total_expenses = rent_amount + food_amount + chai_amount + petrol_amount + electricity_amount
+# Total expenses formula
+total_expenses = rent_amount + food_amount + \
+    chai_amount + petrol_amount + electricity_amount
 amount_per_person = total_expenses / people
-#prininting the values 
+# prininting the values
 print(f"Total expenses: {total_expenses}")
 print(f"Amount each person has to pay: {amount_per_person}")
