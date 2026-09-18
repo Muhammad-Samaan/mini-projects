16f402b702b17ca52a383ef4d8ca149ba5c09984
Muhammad Samaan <msamaan1999@gmail.com>
2026-09-18 09:02:45 +0500

    Refactor and modularize my code (best practice todo)

--- rock_paper_scissor_game.py
+++ rock_paper_scissor_game.py
@@ -1,19 +1,25 @@
 from random import choice
 # why we use tuple instead of list. A:Becz lists are mutable so if we change the value accidently so there will be problem
-
-emojis = {"r": "🪨", "p": "📃", "s": "✂️"}
 choices = ["r", "p", "s"]
-while True:
-    user_choice = input(
-        "Enter your choice Rock , Paper or Scissor (r/p/s): ").lower()
-    if user_choice not in choices:
-        print("Invalid choice!")
-        continue
+emojis = {"r": "🪨", "p": "📃", "s": "✂️"}
 
-    computer_choice = choice(choices)
+
+def get_user_choice():
+    while True:
+        user_choice = input(
+            "Enter your choice Rock , Paper or Scissor (r/p/s): ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid choice!")
+
+
+def display_choices(user_choice, computer_choice):
     print(f"You chose {emojis[user_choice]}")
     print(f"Computer choose {emojis[computer_choice]}")
 
+
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Match Tie")
     elif ((user_choice == "r" and computer_choice == "s") or
@@ -23,7 +29,21 @@ while True:
     else:
         print("You lose")
 
-    player_continue = input("Do you want continue playing (y/n): ").lower()
-    if player_continue == "n":
-        print("Thanks for playing.")
-        break
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        player_continue = input("Do you want continue playing (y/n): ").lower()
+        if player_continue == "n":
+            print("Thanks for playing.")
+            break
+
+
+play_game()
